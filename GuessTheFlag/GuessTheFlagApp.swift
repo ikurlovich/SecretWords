e15e9bcd67f62db849c84faec5ecfe2c6aaa4616
ikurlovich <ikurlovich@icloud.com>
2023-06-07 21:06:46 +0400

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Илья Курлович on 07.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
